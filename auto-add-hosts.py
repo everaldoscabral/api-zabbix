ec1c3881f15e9bec26d3490f2caff2baacbd807c
everaldoscabral <53796085+everaldoscabral@users.noreply.github.com>
2022-01-21 14:09:18 -0300

    Update auto-add-hosts.py
    Ajustes para rodar com python3.

--- auto-add-hosts.py
+++ auto-add-hosts.py
@@ -1,14 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #author: Janssen dos Reis Lima
 
+import sys
+import logging
 from pyzabbix import ZabbixAPI
 import csv
 from progressbar import ProgressBar, Percentage, ETA, ReverseBar, RotatingMarker, Timer
 
+##PARA DEBUG CASO QUEIRA
+#stream = logging.StreamHandler(sys.stdout)
+#stream.setLevel(logging.DEBUG)
+#log = logging.getLogger('pyzabbix')
+#log.addHandler(stream)
+#log.setLevel(logging.DEBUG)
+
 zapi = ZabbixAPI("http://localhost/zabbix")
-zapi.login(user="Admin", password="zabbix")
+zapi.login(user="USER", password="PASSWORD")
+
+# Disable SSL certificate verification
+zapi.session.verify = False
+
+# Specify a timeout (in seconds)
+zapi.timeout = 5.1
 
 arq = csv.reader(open('/tmp/hosts.csv'))
 
@@ -21,7 +36,7 @@ i = 0
 for [hostname,ip] in f:
     hostcriado = zapi.host.create(
         host= hostname,
-        status= 1,
+        status= 0,
         interfaces=[{
             "type": 1,
             "main": "1",
@@ -31,10 +46,10 @@ for [hostname,ip] in f:
             "port": 10050
         }],
         groups=[{
-            "groupid": 2
+            "groupid": 191
         }],
         templates=[{
-            "templateid": 10001
+            "templateid": 10186
         }]
     )
 
@@ -43,4 +58,4 @@ for [hostname,ip] in f:
     bar.update(i)
 
 bar.finish
-print " "
+print()
